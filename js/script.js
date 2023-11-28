b997c018e62ab2e5eaa74d71f2c930d5815c60f0
Paulo Carvalho <142057167+AloPauloCarvalho@users.noreply.github.com>
2023-11-28 16:57:13 -0300

    checkpoint

--- js/script.js
+++ js/script.js
@@ -2,7 +2,5 @@ var typed = new Typed(".auto-type", {
     strings: ["revolucionam", "maximizam", "transformam", "consolidam"],
     typeSpeed: 50,
     backSpeed: 50,
-    bindInputFocusEvents: true,
-    cursorChar: '|',
     loop: true
 })

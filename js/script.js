830bf45989b23f45cce37c023b7cc6bb93c4e096
Paulo Carvalho <142057167+AloPauloCarvalho@users.noreply.github.com>
2023-11-28 13:46:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@ var typed = new Typed(".auto-type", {
     strings: ["revolucionam", "maximizam", "transformam", "consolidam"],
     typeSpeed: 50,
     backSpeed: 50,
+    bindInputFocusEvents: true,
+    cursorChar: '|',
     loop: true
 })
